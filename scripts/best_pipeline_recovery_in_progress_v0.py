a5db874ae4b6351abdddf9c49fd1da1c0c5ad862
SuhasNandiraju <47066425+SuhasNandiraju@users.noreply.github.com>
2021-05-08 00:04:19 -0400

    Add files via upload

---
+++ scripts/best_pipeline_recovery_in_progress_v0.py
@@ -0,0 +1,31 @@
+#92% validation acc
+
+import numpy as np
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import make_pipeline, make_union
+from sklearn.tree import DecisionTreeClassifier
+from tpot.builtins import StackingEstimator, ZeroCount
+from sklearn.impute import SimpleImputer
+from tpot.export_utils import set_param_recursive
+
+tpot_data = pd.read_csv('PATH/TO/DATA/FILE', sep='COLUMN_SEPARATOR', dtype=np.float64)
+features = tpot_data.drop('target', axis=1)
+training_features, testing_features, training_target, testing_target = \
+            train_test_split(features, tpot_data['target'], random_state=42)
+
+imputer = SimpleImputer(strategy="median")
+imputer.fit(training_features)
+training_features = imputer.transform(training_features)
+testing_features = imputer.transform(testing_features)
+
+exported_pipeline = make_pipeline(
+    StackingEstimator(estimator=DecisionTreeClassifier(criterion="gini", max_depth=2, min_samples_leaf=17, min_samples_split=10)),
+    ZeroCount(),
+    RandomForestClassifier(bootstrap=False, criterion="entropy", max_features=0.1, min_samples_leaf=1, min_samples_split=4, n_estimators=100)
+)
+set_param_recursive(exported_pipeline.steps, 'random_state', 42)
+
+exported_pipeline.fit(training_features, training_target)
+results = exported_pipeline.predict(testing_features)
